07c0c0f2ebc726b4108dfbf7168be0cab959ff3e
Anatolyi Ovrutskyi <a.s.ovrutskyi@gmail.com>
2019-05-08 16:11:20 +0300

    install template like slpit for parts

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -4,12 +4,25 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Page;
+use App\Service;
+use App\Portfolio;
+use App\People;
+
+
+
 class IndexController extends Controller
 {
     public function execute(Request $request) {
 
+    	$pages = Page::all();
+    	$portfolios = Portfolio::get(array('name', 'filter', 'images'));
+    	$services = Service::all();
+    	$peoples = People::all();
 
-    	return view('layouts.side');
+    	// dd($peoples);
+
+    	return view('site.index');
 
     }
 }
--- resources/views/site/content.blade.php
+++ resources/views/site/content.blade.php
@@ -1,51 +1,4 @@
-<!doctype html>
-<html>
-<head>
-<meta charset="utf-8">
-<meta name="viewport" content="width=device-width, maximum-scale=1">
-<title>Unique</title>
-<link rel="icon" href="{{ asset('assets/favicon.png') }}" type="image/png">
-<link href="{{ asset('assets/css/bootstrap.min.css') }}" rel="stylesheet" type="text/css">
-<link href="{{ asset('assets/css/style.css') }}" rel="stylesheet" type="text/css"> 
-<link href="{{ asset('assets/css/font-awesome.css') }}" rel="stylesheet" type="text/css"> 
-<link href="{{ asset('assets/css/animate.css') }}" rel="stylesheet" type="text/css">
- 
-<!--[if lt IE 9]>
-    <script src="{{ asset('assets/js/respond-1.1.0.min.js') }}"></script>
-    <script src="{{ asset('assets/js/html5shiv.js') }}"></script>
-    <script src="{{ asset('assets/js/html5element.js') }}"></script>
-<![endif]-->
- 
-</head>
-<body>
 
-<!--Header_section-->
-<header id="header_wrapper">
-  <div class="container">
-    <div class="header_box">
-      <div class="logo"><a href="#"><img src="{{ asset('assets/img/logo.png') }}" alt="logo"></a></div>
-	  <nav class="navbar navbar-inverse" role="navigation">
-      <div class="navbar-header">
-        <button type="button" id="nav-toggle" class="navbar-toggle" data-toggle="collapse" data-target="#main-nav"> <span class="sr-only">Toggle navigation</span> <span class="icon-bar"></span> <span class="icon-bar"></span> <span class="icon-bar"></span> </button>
-        </div>
-	    <div id="main-nav" class="collapse navbar-collapse navStyle">
-			<ul class="nav navbar-nav" id="mainNav">
-			  <li class="active"><a href="#hero_section" class="scroll-link">Home</a></li>
-			  <li><a href="#aboutUs" class="scroll-link">About Us</a></li>
-			  <li><a href="#service" class="scroll-link">Services</a></li>
-			  <li><a href="#Portfolio" class="scroll-link">Portfolio</a></li>
-			  <li><a href="#clients" class="scroll-link">Clients</a></li>
-			  <li><a href="#team" class="scroll-link">Team</a></li>
-			  <li><a href="#contact" class="scroll-link">Contact</a></li>
-			</ul>
-      </div>
-	 </nav>
-    </div>
-  </div>
-</header>
-<!--Header_section--> 
-
-<!--Hero_Section-->
 <section id="hero_section" class="top_cont_outer">
   <div class="hero_wrapper">
     <div class="container">
@@ -58,15 +11,14 @@
               <a href="#service" class="read_more2">Read more</a> </div>
           </div>
           <div class="col-lg-7 col-sm-5">
-			<img src="{{ asset('assets/img/main_device_image.png') }}" class="zoomIn wow animated" alt="" />
-		  </div>
+      <img src="{{ asset('assets/img/main_device_image.png') }}" class="zoomIn wow animated" alt="" />
+      </div>
         </div>
       </div>
     </div>
   </div>
 </section>
 <!--Hero_Section--> 
-
 <section id="aboutUs"><!--Aboutus-->
 <div class="inner_wrapper">
   <div class="container">
@@ -434,15 +386,3 @@
     <div class="footer_bottom"><span>Copyright © 2014,    Template by <a href="http://webthemez.com">WebThemez.com</a>. </span> </div>
   </div>
 </footer>
-
-<script type="text/javascript" src="{{ asset('assets/js/jquery-1.11.0.min.js') }}"></script>
-<script type="text/javascript" src="{{ asset('assets/js/bootstrap.min.js') }}"></script>
-<script type="text/javascript" src="{{ asset('assets/js/jquery-scrolltofixed.js') }}"></script>
-<script type="text/javascript" src="{{ asset('assets/js/jquery.nav.js') }}"></script> 
-<script type="text/javascript" src="{{ asset('assets/js/jquery.easing.1.3.js') }}"></script>
-<script type="text/javascript" src="{{ asset('assets/js/jquery.isotope.js') }}"></script>
-<script type="text/javascript" src="{{ asset('assets/js/wow.js') }}"></script> 
-<script type="text/javascript" src="{{ asset('assets/js/custom.js') }}"></script>
-
-</body>
-</html>
