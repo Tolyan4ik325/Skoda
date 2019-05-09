1800abfa5a0163487071f349ff96fed6b6eda48c
Anatolyi Ovrutskyi <a.s.ovrutskyi@gmail.com>
2019-05-09 22:42:54 +0300

    services

--- resources/views/site/content.blade.php
+++ resources/views/site/content.blade.php
@@ -51,68 +51,43 @@
 
 @endforeach
 
-
-
 @endif
 
-
-
-
+@if(isset($services) && is_object($services))
 <!--Service-->
 <section  id="service">
   <div class="container">
     <h2>Services</h2>
     <div class="service_wrapper">
-      <div class="row">
-        <div class="col-lg-4">
+
+      @foreach($services As $k=>$service)
+
+          @if($k == 0 || $k%3 == 0)
+              <div class="row {{ ($k != 0) ? 'borderTop' : ''}}">
+
+          @endif
+         <div class="col-lg-4 {{ ($k%3 > 0 ? 'borderLeft' : '')}} {{ $k > 2 ? 'mrgTop' : ''}}">   
           <div class="service_block">
-            <div class="service_icon delay-03s animated wow  zoomIn"> <span><i class="fa fa-android"></i></span> </div>
-            <h3 class="animated fadeInUp wow">Android</h3>
-            <p class="animated fadeInDown wow">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text.</p>
+            <div class="service_icon delay-03s animated wow  zoomIn"> <span><i class="fa {{$service->icon }}"></i></span> </div>
+            <h3 class="animated fadeInUp wow">{{$service->name}}</h3>
+            <p class="animated fadeInDown wow">{{$service->text}}</p>
           </div>
         </div>
-        <div class="col-lg-4 borderLeft">			
-          <div class="service_block">
-            <div class="service_icon icon2  delay-03s animated wow zoomIn"> <span><i class="fa fa-apple"></i></span> </div>
-            <h3 class="animated fadeInUp wow">Apple IOS</h3>
-            <p class="animated fadeInDown wow">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text.</p>
-          </div>
-        </div>
-        <div class="col-lg-4 borderLeft">
-          <div class="service_block">
-            <div class="service_icon icon3  delay-03s animated wow zoomIn"> <span><i class="fa fa-html5"></i></span> </div>
-            <h3 class="animated fadeInUp wow">Design</h3>
-            <p class="animated fadeInDown wow">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text.</p>
-          </div>
-        </div>
-      </div>
-	   <div class="row borderTop">
-        <div class="col-lg-4 mrgTop">
-          <div class="service_block">
-            <div class="service_icon delay-03s animated wow  zoomIn"> <span><i class="fa fa-dropbox"></i></span> </div>
-            <h3 class="animated fadeInUp wow">Concept</h3>
-            <p class="animated fadeInDown wow">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text.</p>
-          </div>
-        </div>
-        <div class="col-lg-4 borderLeft mrgTop">
-          <div class="service_block">
-            <div class="service_icon icon2  delay-03s animated wow zoomIn"> <span><i class="fa fa-slack"></i></span> </div>
-            <h3 class="animated fadeInUp wow">User Research</h3>
-            <p class="animated fadeInDown wow">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text.</p>
-          </div>
-        </div>
-        <div class="col-lg-4 borderLeft mrgTop">
-          <div class="service_block">
-            <div class="service_icon icon3  delay-03s animated wow zoomIn"> <span><i class="fa fa-users"></i></span> </div>
-            <h3 class="animated fadeInUp wow">User Experience</h3>
-            <p class="animated fadeInDown wow">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text.</p>
-          </div>
-        </div>
-      </div>
+
+        @if(($k + 1)%3 == 0) 
+            </div>
+        @endif
+
+      @endforeach
+   
     </div>
   </div>
 </section>
 <!--Service-->
+@endif
+
+
+
 
 
 
