cf6adbd6969e646373f0ce659e08113f1f9f12b0
Anatolyi Ovrutskyi <a.s.ovrutskyi@gmail.com>
2019-05-10 17:52:01 +0300

    last commit

---
+++ app/Http/Controllers/PageController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Page;
+
+class PageController extends Controller
+{
+    //
+
+    public function execute($alias) {
+
+    	// $alias = FALSE;
+
+    	if(!$alias) {
+    		abort(404);
+
+    	}
+
+    	if(view()->exists('site.page')) {
+
+    		// Where 'alias' = $alias;
+
+    		$page = Page::where('alias', strip_tags($alias))->first();
+
+    		$data = [
+
+    			'title' => $page->name,
+    			'page' => $page
+
+    		];
+
+    		return view('site.page', $data);
+
+    	}
+    	else {
+    		abort(404);
+    	}
+
+    }
+}
