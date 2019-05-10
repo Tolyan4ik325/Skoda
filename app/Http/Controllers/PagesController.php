b9cb7262ca25108900df64b0a97e62ac7f050bca
Anatolyi Ovrutskyi <a.s.ovrutskyi@gmail.com>
2019-05-10 20:36:43 +0300

    editor

---
+++ app/Http/Controllers/PagesController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+// use App\Page;
+
+class PagesController extends Controller
+{
+    //
+
+    public function execute() {
+    	if(view()->exists('admin.pages')) {
+    		$pages = \App\Page::all();
+
+    		$data = [
+
+    				'title' => 'Страницы',
+    				'pages' => $pages
+
+    		];
+
+    		return view('admin.pages', $data);
+    	}
+
+    	abort(404);
+    }
+}
---
+++ resources/views/admin/content_pages.blade.php
@@ -0,0 +1,44 @@
+
+
+@if($pages)
+
+<table class="table table-hover table-stripped">
+	<thead>
+		<tr>
+			<th>№ п/п</th>
+			<th>Имя</th>
+			<th>Псевдоним</th>
+			<th>Текст</th>
+			<th>Дата создания</th>
+			<!-- <th>Ссылка</th> -->
+			<th>Удалить</th>
+		</tr>
+	</thead>
+	<tbody>
+		@foreach($pages as $k => $page)
+			<tr>
+				
+				<td>{{ $page->id }}</td>
+				<td>{!! Html::link(route('pagesEdit', ['page'=>$page->id]), $page->name, ['alt'=>$page->name] )!!}</td>
+				<td>{{ $page->alias }}</td>
+				<td>{{ $page->text }}</td>
+				<td>{{ $page->created_at }}</td>
+
+				<td>
+				{!! Form::open(['url'=>route('pagesEdit', ['page'=>$page->id]), 'class'=>'form-horizontal', 'method' => 'POST']) !!}
+
+					{!! Form::hidden('action', 'delete') !!}
+
+					{!! Form::button('Удалить', ['class'=> 'btn btn-danger', 'type'=>'submit']) !!}
+
+				{!! Form::close() !!}
+				</td>
+			</tr>
+		@endforeach
+	</tbody>
+</table>
+
+
+@endif
+</div>
+{!! Html::link(route('pagesAdd'),'Новая страница')!!}
